c171c3e534db236cd1e0663cac59a0d0be52d5db
WATUTHANTHIRIGE LASHANE YURESHKA ALWIS <133086262+Yureshka02@users.noreply.github.com>
2024-09-18 00:08:45 +0530

    fixed the decode dunction / NullpointException (current)

--- src/MorseCodeApp.java
+++ src/MorseCodeApp.java
@@ -2,81 +2,29 @@ public class MorseCodeApp {
     public static void main(String[] args) {
         MorseCodeTree tree = new MorseCodeTree();
 
-        //inserting the morse code and the character into the tree
+        // Inserting the Morse code and the character into the tree
         String[] codes = {
-                ".-",
-                "-...",
-                "-.-.",
-                "-..",
-                ".",
-                "..-.",
-                "--.",
-                "....",
-                "..",
-                ".---",
-                "-.-",
-                ".-..",
-                "--",
-                "-.",
-                "---",
-                ".--.",
-                "--.-",
-                ".-.",
-                "...",
-                "-",
-                "..-",
-                "...-",
-                ".--",
-                "-..-",
-                "-.--",
-                "--..",
-
-
+                ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+                "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
         };
 
         char[] characters = {
-                'A',
-                'B',
-                'C',
-                'D',
-                'E',
-                'F',
-                'G',
-                'H',
-                'I',
-                'J',
-                'K',
-                'L',
-                'M',
-                'N',
-                'O',
-                'P',
-                'Q',
-                'R',
-                'S',
-                'T',
-                'U',
-                'V',
-                'W',
-                'X',
-                'Y',
-                'Z',
-
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
         };
 
-        //inserting the morse code and the character into the tree
+        // Inserting the Morse code and the character into the tree
         for (int i = 0; i < codes.length; i++) {
             tree.insert(codes[i], characters[i]);
         }
-        //printing the morse code and the character
+
         // Print the Morse code dictionary using in-order traversal
         System.out.println("Morse Code Dictionary (In-order Traversal):");
         tree.inorderPrint(tree.getRoot(), "");
         System.out.println();
 
-
         // Decode the Morse code message
-        String morseCode ="/ -- . ..-. ...- --- .-. .--./ . .... -/ . -... ..-. --- .../ - ... .-. .. .-..";
+        String morseCode = ". --. .- ... ... . -- / . .-.. .--. -- .- ... / .- / ... .. / ... .. .... -";
         String decodedMessage = tree.decode(morseCode);
         System.out.println("Decoded Message: " + decodedMessage);
 
@@ -85,4 +33,3 @@ public class MorseCodeApp {
         System.out.println("Reversed Message: " + reversedMessage);
     }
 }
-//-.-- --- ..- / .-.. --- ...- . / ... --- ...
